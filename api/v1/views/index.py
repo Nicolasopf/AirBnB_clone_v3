c41dc7e153bd449b847f8c3cae534b3f13897221
Xvaria <2349@holbertonschool.com>
2021-05-09 22:41:46 +0000

    [Updated] Module jsonify and add comments

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+'''build api routes'''
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status')
 def json_return():
-    return {"status": "OK"}
+    '''return a json with status'''
+    return jsonify({'status':"OK"})
